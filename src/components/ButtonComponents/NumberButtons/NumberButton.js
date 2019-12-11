574c1191e58725e5ad431aa0e03995520c4d9eab
George Davila <georgedavila@Georges-MacBook-Pro.local>
2019-12-10 22:36:59 -0500

    finished mvp

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  console.log(props);
+
   return (
-    <>
+    <div className = {`btn  btn-number ${props === '0' ? 'double' : '' }`}>
+    <button onClick = {() => props.addNumber(props.text)}>
+      {props.text}
+    </button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </div>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  console.log(props);
   return (
-    <>
+    <div className = "btn btn-special ">
+      <button>
+        {props.text}
+      </button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </div>
   );
 };
+
+export default SpecialButton;
